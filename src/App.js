9d2eb87a0bdbb5708f011b1a59ff9993e79f6dc2
KareemMamdouh <kareem.mamdouh97@yahoo.com>
2021-02-06 21:16:34 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Test from 'pages/Test/Test'
 import './App.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './i18n'
+import { Router, Switch, Route } from 'react-router-dom'
+import history from 'redux/_helpers/history'
 
 require('dotenv').config()
 
 function App() {
   return (
     <div className="App">
-      <Test />
+      <Router history={history}>
+        <Route exact path="/" component={Test} />
+      </Router>
     </div>
   )
 }
